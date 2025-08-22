4ff9601f97b80aa1725d3bb080687b848efc4791
joaquinzxy <joaquindesosa@gmail.com>
2025-08-22 17:16:12 -0300

    dynamic age

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -7,6 +7,16 @@ export const About = () => {
 
   const [t, i18n] = useTranslation('global');
 
+  const birthdate = new Date('1998-12-28');
+  const today = new Date();
+  let age = today.getFullYear() - birthdate.getFullYear();
+  const hasHadBirthdayThisYear =
+    today.getMonth() > birthdate.getMonth() ||
+    (today.getMonth() === birthdate.getMonth() && today.getDate() >= birthdate.getDate());
+  if (!hasHadBirthdayThisYear) {
+    age -= 1;
+  }
+
   return (
     <Box>
       <Box gap={'1.5rem'} display={'flex'} flexDir={'column'}>
@@ -14,7 +24,7 @@ export const About = () => {
           {t('about.section_title')}
         </Heading>
         <p>
-          {t('about.paragraph_1')}
+          {t('about.paragraph_1').replace('%%age%%', age)}
         </p>
         <p>
           {t('about.paragraph_2')}
